7e4f7850eea3627e5fc0ccb094e68fdbd1bf7ea6
Yousef <yousefdhm@gmail.com>
2024-05-15 11:13:11 +0300

    changed file structure

--- extensions/tmtv/src/Panels/index.tsx
+++
@@ -1,4 +0,0 @@
-import PanelPetSUV from './PanelPetSUV';
-import PanelROIThresholdSegmentation from './PanelROIThresholdSegmentation';
-
-export { PanelPetSUV, PanelROIThresholdSegmentation };
--- platform/ui/src/components/AdvancedToolbox/index.js
+++
@@ -1,2 +0,0 @@
-import AdvancedToolbox from './AdvancedToolbox';
-export default AdvancedToolbox;
